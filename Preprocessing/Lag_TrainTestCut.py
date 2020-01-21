d933ea7b299a3bf4fa9f228aabf639f04b8fcaeb
claircocoanut <58301352+claircocoanut@users.noreply.github.com>
2020-01-22 00:34:43 +0800

    update preprocessing

--- Preprocessing/Lag_TrainTestCut.py
+++ Preprocessing/Lag_TrainTestCut.py
@@ -1,10 +1,9 @@
-from sqlalchemy import create_engine
 import pandas as pd
+from sqlalchemy import create_engine
+
 
 def check_correlation(df, threshold=0.9):
     # find high correlated items -> excel
-
-    ex_corr_col = []
 
     def high_corr(df, threshold=0.9):
         corr = df.corr().abs()
@@ -15,30 +14,6 @@ def check_correlation(df, threshold=0.9):
         so = so.loc[(so['v1'] != so['v2']) & (so['corr'] > threshold)].drop_duplicates(subset=['v1', 'v2'])
         return so
 
-    def del_corr():
-        # Set of all the names of deleted columns
-        from collections import Counter
-        items = high_corr_df['v1'].to_list() + high_corr_df['v2'].to_list()
-        items_occurence = Counter(items).keys()
-
-        col_corr = set()
-        for i in range(len(corr_matrix.columns)):
-            for j in range(i):
-                if (corr_matrix.iloc[i, j] >= threshold) and (corr_matrix.columns[j] not in col_corr):
-                    colname = corr_matrix.columns[i]  # getting the name of column
-                    col_corr.add(colname)
-        del_corr = col_corr - set(ex_corr_col)
-
-        # DataFrame of high correlated, description, and values
-        del_value = high_corr_df.loc[(high_corr_df['v1'].isin(del_corr)) | (high_corr_df['v2'].isin(del_corr))]
-        del_col_ie = pd.merge(del_value, format_map, left_on=['v1'], right_on=['abbreviation'], how='left')
-        del_col_ie = pd.merge(del_col_ie, format_map, left_on=['v2'], right_on=['abbreviation'], how='left')
-        del_col_ie = del_col_ie.loc[((del_col_ie['abbreviation_x'].isin(del_corr))) &
-                                    (~(del_col_ie['abbreviation_y'].isin(del_corr)))].sort_values(
-            by='abbreviation_x').iloc[:, 2:]
-        return del_corr, del_col_ie
-
-    corr_matrix = df.corr()
     high_corr_df = high_corr(df)
     print(high_corr_df)
 
@@ -54,24 +29,36 @@ def merge_dep_macro(df, dependent_variable='epspxq_nextq_abs'):
     return df_1, macro_lst
 
 
-def add_lag(df, col):
-    def namelag(lst, i):
-        return [(k + '_lag' + str(i).zfill(2)) for k in lst]
+def add_lag(df):
+
+    col = df.iloc[:, 3:].columns
 
     for i in range(19):
         print(i)
-        df[namelag(col, i + 1)] = df.groupby('gvkey').shift(i + 1)[col]
+        namelag = [(k + '_lag' + str(i+1).zfill(2)) for k in col]
+        df[namelag] = df.iloc[:, 3:].groupby('gvkey').shift(i + 1)[col]
+
     return df
 
+def cut_test_train(df):
 
-def cut_train_test(df, testing_cqtr):
-    df_train = df.loc[df['datacqtr_no'].isin(range(testing_cqtr - 20, testing_cqtr, 1))]
-    df_test = df.loc[df['datacqtr_no'] == testing_cqtr]
-    return df_train, df_test
+    def datacqtr_to_no(df):
+        cqtr = pd.DataFrame(set(df['datacqtr']), columns=['datacqtr']).sort_values(by=['datacqtr']).reset_index(
+            drop=True).reset_index(drop=False).set_index('datacqtr')
+        df['datacqtr_no'] = df.datacqtr.map(cqtr['index'])
+        print(cqtr.loc['2008Q1'])
+        return df
 
+    df = datacqtr_to_no(df)
+    test_train_dict = {}
+    for testing_cqtr in range(113, 154) # datacqtr_no for 2008Q1 ~ (2018Q4 + 1)
+        test_train_dict[testing_cqtr - 113] = {}
+        test_train_dict[testing_cqtr - 113]['test'] = df.loc[df['datacqtr_no'] == testing_cqtr]
+        test_train_dict[testing_cqtr - 113]['train'] = df.loc[df['datacqtr_no'].isin(range(testing_cqtr - 20, testing_cqtr, 1))]
 
-if __name__ == "__main__":
+    return test_train_dict
 
+def full_running_cut():
 
     # import engine, select variables, import raw database
     try:
@@ -83,22 +70,25 @@ if __name__ == "__main__":
         engine = create_engine(db_string)
         main = pd.read_sql('SELECT * FROM main_forward', engine) # change forward/rolling for two different fillna version
 
-    print(main.isnull().sum().sum())
-    print(main.shape)
-    check_correlation(main.iloc[:,2:])
+    # check_correlation(main.iloc[:,2:])
 
-    del_corr = ['xsgaq_qoq', 'gdwlq_log', 'cogsq_qoq']  # same for both forward & rolling version
+    # 1. delete high correlation items
+    del_corr = ['xsgaq_qoq', 'gdwlq_atq', 'cogsq_qoq']  # same for both forward & rolling version
     main = main.drop(main[del_corr], axis=1)
 
-    # main = drop_nonseq(main)
-    # main, macro_lst = merge_dep_macro(raw)  # add dependent variable & macro variables to main
-    #
-    # num_col = numeric_dict['YoY'] + numeric_dict['QoQ'] + numeric_dict['Abs'] + numeric_dict['Log']
-    # main_lag = add_lag(df, num_col).dropna(how='any', axis=0)
-    #
-    # pca_dict = {}
-    # pca_dict['train'] = {}
-    # pca_dict['test'] = {}
-    #
-    #     for i in range(111):
-    #         pass
+    # 2. add 20 lagging factors for each variable
+    main_lag = add_lag(main).dropna(how='any', axis=0)
+
+    # 3. add dependent variable & macro variables to main
+    main, macro_lst = merge_dep_macro(main_lag)
+
+    # 4. cut training, testing set
+    test_train_dict = cut_test_train(main)
+
+    return test_train_dict
+
+if __name__ == "__main__":
+
+    # actual running scripts see def above -> for import
+    full_running_cut()
+
--- Preprocessing/Miscellaneous.py
+++ Preprocessing/Miscellaneous.py
@@ -9,4 +9,45 @@ def unstack_selection():
     format_map_df.columns = ['format', 'name', 'selection']
     format_map_df.to_csv('format_map_selection.csv')
 
+def check_correlation_delete(df, threshold=0.9):
+    # find high correlated items -> excel
+
+    ex_corr_col = []
+
+    def high_corr(df, threshold=0.9):
+        corr = df.corr().abs()
+        so = corr.unstack().reset_index()
+        print(so)
+        # so = so.sort_values(kind="quicksort", ascending=False).to_frame().reset_index()
+        so.columns = ['v1', 'v2', 'corr']
+        so = so.loc[(so['v1'] != so['v2']) & (so['corr'] > threshold)].drop_duplicates(subset=['v1', 'v2'])
+        return so
+
+    def del_corr():
+        # Set of all the names of deleted columns
+        from collections import Counter
+        items = high_corr_df['v1'].to_list() + high_corr_df['v2'].to_list()
+        items_occurence = Counter(items).keys()
+
+        col_corr = set()
+        for i in range(len(corr_matrix.columns)):
+            for j in range(i):
+                if (corr_matrix.iloc[i, j] >= threshold) and (corr_matrix.columns[j] not in col_corr):
+                    colname = corr_matrix.columns[i]  # getting the name of column
+                    col_corr.add(colname)
+        del_corr = col_corr - set(ex_corr_col)
+
+        # DataFrame of high correlated, description, and values
+        del_value = high_corr_df.loc[(high_corr_df['v1'].isin(del_corr)) | (high_corr_df['v2'].isin(del_corr))]
+        del_col_ie = pd.merge(del_value, format_map, left_on=['v1'], right_on=['abbreviation'], how='left')
+        del_col_ie = pd.merge(del_col_ie, format_map, left_on=['v2'], right_on=['abbreviation'], how='left')
+        del_col_ie = del_col_ie.loc[((del_col_ie['abbreviation_x'].isin(del_corr))) &
+                                    (~(del_col_ie['abbreviation_y'].isin(del_corr)))].sort_values(
+            by='abbreviation_x').iloc[:, 2:]
+        return del_corr, del_col_ie
+
+    corr_matrix = df.corr()
+    high_corr_df = high_corr(df)
+    print(high_corr_df)
+
 if __name__ == "__main__":
