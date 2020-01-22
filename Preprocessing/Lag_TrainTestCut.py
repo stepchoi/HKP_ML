9f9d6a9807ac10d4f8914b23cf92b3eff470bbde
claircocoanut <58301352+claircocoanut@users.noreply.github.com>
2020-01-22 17:50:41 +0800

    update preprocessing

--- Preprocessing/Lag_TrainTestCut.py
+++ Preprocessing/Lag_TrainTestCut.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from tqdm import tqdm
 
 
 def check_correlation(df, threshold=0.9):
@@ -17,44 +18,51 @@ def check_correlation(df, threshold=0.9):
     high_corr_df = high_corr(df)
     print(high_corr_df)
 
+def add_lag(df):
+    print('----- adding lag -----')
+    col = df.iloc[:, 3:].columns
 
-def merge_dep_macro(df, dependent_variable='epspxq_nextq_abs'):
+    for i in tqdm(range(19)):
+        namelag = [(k + '_lag' + str(i+1).zfill(2)) for k in col]
+        df[namelag] = df.groupby('gvkey').shift(i + 1)[col]
+    return df
+
+
+def merge_dep_macro(df, dependent_variable='next1_abs'): #dependent variables includes: next1_abs, next4_abs, epspxq_qoq, epspxq_yoy
+    db_string = 'postgres://postgres:DLvalue123@hkpolyu.cgqhw7rofrpo.ap-northeast-2.rds.amazonaws.com:5432/postgres'
+    engine = create_engine(db_string)
+
+    print('----- adding macro & dependent variable -----')
     macro = pd.read_sql("SELECT * FROM macro_clean", engine)
+    macro['datacqtr'] = ['{}Q{}'.format(y, q) for q, y in zip(macro['cquarter'], macro['cyear'])]
     macro_lst = macro.columns[2:]
-    dep = pd.read_sql('SELECT gvkeydatafqtr, ' + dependent_variable + ', selected FROM main_dependent', engine)
 
-    df_1 = pd.merge(df, dep, on=['gvkeydatafqtr'], how='left')
-    df_1 = pd.merge(df_1, macro, on=['cyear', 'cquarter'], how='left')
+    dep = pd.read_sql('SELECT gvkey, datacqtr, ' + dependent_variable + ' selected FROM epspxq', engine)
+
+    for col in ['gvkey', 'datacqtr']:
+        df[col] = df[col].astype(str)
+        dep[col] = dep[col].astype(str)
+
+    df_1 = pd.merge(df, dep, on=['gvkey', 'datacqtr'], how='left')
+    df_1 = pd.merge(df_1, macro, on=['datacqtr'], how='left')
 
     return df_1, macro_lst
 
 
-def add_lag(df):
-
-    col = df.iloc[:, 3:].columns
-
-    for i in range(19):
-        print(i)
-        namelag = [(k + '_lag' + str(i+1).zfill(2)) for k in col]
-        df[namelag] = df.iloc[:, 3:].groupby('gvkey').shift(i + 1)[col]
-
-    return df
-
 def cut_test_train(df):
 
     def datacqtr_to_no(df):
-        cqtr = pd.DataFrame(set(df['datacqtr']), columns=['datacqtr']).sort_values(by=['datacqtr']).reset_index(
-            drop=True).reset_index(drop=False).set_index('datacqtr')
+        cqtr = pd.DataFrame(['{}Q{}'.format(y,q) for y in range(1979, 2020) for q in range(1,5)]).reset_index().set_index(0)
         df['datacqtr_no'] = df.datacqtr.map(cqtr['index'])
-        print(cqtr.loc['2008Q1'])
         return df
 
+    print('----- start cutting testing/training set -----')
     df = datacqtr_to_no(df)
     test_train_dict = {}
-    for testing_cqtr in range(113, 154) # datacqtr_no for 2008Q1 ~ (2018Q4 + 1)
-        test_train_dict[testing_cqtr - 113] = {}
-        test_train_dict[testing_cqtr - 113]['test'] = df.loc[df['datacqtr_no'] == testing_cqtr]
-        test_train_dict[testing_cqtr - 113]['train'] = df.loc[df['datacqtr_no'].isin(range(testing_cqtr - 20, testing_cqtr, 1))]
+    for testing_cqtr in tqdm(range(116, 157)): # datacqtr_no for 2008Q1 = 116 ~ (2018Q4 = 156 + 1)
+        test_train_dict[testing_cqtr - 116] = {}
+        test_train_dict[testing_cqtr - 116]['test'] = df.loc[df['datacqtr_no'] == testing_cqtr]
+        test_train_dict[testing_cqtr - 116]['train'] = df.loc[df['datacqtr_no'].isin(range(testing_cqtr - 20, testing_cqtr, 1))]
 
     return test_train_dict
 
@@ -84,6 +92,7 @@ def full_running_cut():
 
     # 4. cut training, testing set
     test_train_dict = cut_test_train(main)
+    print(test_train_dict)
 
     return test_train_dict
 
