8eb27da028a59c66c69009b78c8634f05e815636
claircocoanut <58301352+claircocoanut@users.noreply.github.com>
2020-02-22 21:41:24 +0800

    Update preprocessing

--- (AE) import & sampling.py
+++ (AE) import & sampling.py
@@ -1,13 +1,12 @@
-from Preprocessing.LoadData import (sample_from_main)
+from Preprocessing.LoadData import (load_data, clean_set)
 
+'''
 part_dict = sample_from_main(part=5)  # part_dict[0], part_dict[1], ... would be arrays after standardization
 print(part_dict)
 
 for i in part_dict.keys():
     pass
-
-
-
+'''
 
 main = load_data(sql_version=False)  # change sql_version -> True if trying to run this code through Postgres Database
 period_1 = dt.datetime(2008, 3, 31)
