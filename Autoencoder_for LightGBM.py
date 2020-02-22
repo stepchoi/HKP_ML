13b0c2d5faacd0bb5bef783fe6324713d33199fb
fluting <46188281+fluting@users.noreply.github.com>
2020-02-23 06:57:37 +0800

    Add files via upload

--- Autoencoder_for LightGBM.py
+++ Autoencoder_for LightGBM.py
@@ -8,7 +8,7 @@ from keras.models import Sequential, Model
 from keras.utils.vis_utils import plot_model
 
 
-def AE(data):
+def AE_fitting(training_x):
 
     model = Sequential()
     model.add(Dense(units=1800, activation='tanh', name='en1', input_shape=[3169]))
@@ -21,19 +21,26 @@ def AE(data):
     # extract compressed feature
     model.compile(optimizer='adam', loss='mae')
 
-    model.fit(data, data, batch_size=2000, epochs=50)
+    model.fit(training_x, training_x, batch_size=2000, epochs=50)
     feature_model = Model(inputs=model.input, outputs=model.get_layer(name='en2').output)
-    features = feature_model.predict(data)
-    print('feature shape=', features.shape)
 
-    return features
+    return feature_model
+
+def AE_predict(x, feature_model):
+
+    compressed_x = feature_model.predict(x)
+    print('feature shape=', compressed_x.shape)
+
+    return compressed_x
 
 
 
 if __name__ == "__main__":
 
-    data = pd.read_csv('trainingset0.csv', index_col=0)
+    training_x = pd.read_csv('trainingset0.csv', index_col=0)
+    #testing_x = pd.read_csv('trainingset1.csv', index_col=0)
 
-    features = AE(data)
-    print(features)
-    print(features.shape)
+    feature_model = AE_fitting(training_x)
+    training_compressed_x = AE_predict(training_x, feature_model)
+    #testing_compressed_x = AE_predict(testing_x, feature_model)
+    print(training_compressed_x.shape)
