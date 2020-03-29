944fb1190fb3c24fd6988523ebfbfea83a817de4
claircocoanut <58301352+claircocoanut@users.noreply.github.com>
2020-03-29 23:15:22 +0800

    Update lightgbm

--- LoadDataRNN.py
+++ LoadDataRNN.py
@@ -1,37 +1,35 @@
-import time
+import datetime as dt
 
+import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
 
 
-def main(sql_version=False, save_csv=False):
-
-    print('-------------- start load data --------------')
-    start = time.time()
+def main(sql_version=False):
 
     if sql_version is True:  # sql version read TABLE from Postgre SQL
         db_string = 'postgres://postgres:DLvalue123@hkpolyu.cgqhw7rofrpo.ap-northeast-2.rds.amazonaws.com:5432/postgres'
         engine = create_engine(db_string)
         main = pd.read_sql('SELECT * FROM main', engine)
     else:  # local version read TABLE from local csv files -> faster
-        main = pd.read_csv('Hyperopt_LightGBM/main.csv')
+        main = pd.read_csv('Hyperopt_LightGBM/niq_main.csv')
         engine = None
         print('local version running - main')
 
-    main = main.dropna(how='any',axis=0)    # drop rows with NaN on important columns e.g. niq, atq...
+    main['datacqtr'] = pd.to_datetime(main['datacqtr'],format='%Y-%m-%d')
+    main = main.dropna(how='any')
+    main = main.loc[(main['datacqtr']<= dt.datetime(2019, 1, 1))&(main['datacqtr']>= dt.datetime(1983, 1, 1))]
 
-    rnn_dict = {}
+    arr=[]
+    for name, g in main.groupby('datacqtr'):
+        print(name)
+        g = g.set_index('gvkey')
+        df = pd.DataFrame(index=list(set(main['gvkey'])))
+        df0 = pd.merge(df, g, left_index=True, right_index=True,how='outer').filter(main.columns[2:])
+        arr.append(np.array(df0))
 
-    for datacqtr in set(main['datacqtr']):
-        rnn_dict[datacqtr] = main.loc[main['datacqtr']==datacqtr].set_index('gvkey')  # set gvkey as index for each quarterly DataFrame
-
-        if save_csv == True:    # if set save_csv==True, save each quarterly df into individual csv, named after 'datacqtr'
-            rnn_dict[datacqtr].to_csv(datacqtr+'.csv', index=False)
-
-    end = time.time()
-    print('LoadDataRNN running time {}'.format(end - start))    # enrire processing takes about 20s
-
-    return rnn_dict
+    arr3d = np.array(arr)
+    return arr3d
 
 if __name__ == '__main__':
-    rnn_dict = main(sql_version=False, save_csv=False)
+    arr3d = main()
