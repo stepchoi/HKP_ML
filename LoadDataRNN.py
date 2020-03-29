6099e44d9d460703ec151c3209ee904f3c64ac20
claircocoanut <58301352+claircocoanut@users.noreply.github.com>
2020-03-29 23:23:53 +0800

    Update lightgbm

--- LoadDataRNN.py
+++ LoadDataRNN.py
@@ -2,6 +2,7 @@ import datetime as dt
 
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 from sqlalchemy import create_engine
 
 
@@ -12,13 +13,16 @@ def main(sql_version=False):
         engine = create_engine(db_string)
         main = pd.read_sql('SELECT * FROM main', engine)
     else:  # local version read TABLE from local csv files -> faster
-        main = pd.read_csv('Hyperopt_LightGBM/niq_main.csv')
+        main = pd.read_csv('Hyperopt_LightGBM/main.csv')
         engine = None
         print('local version running - main')
 
     main['datacqtr'] = pd.to_datetime(main['datacqtr'],format='%Y-%m-%d')
     main = main.dropna(how='any')
     main = main.loc[(main['datacqtr']<= dt.datetime(2019, 1, 1))&(main['datacqtr']>= dt.datetime(1983, 1, 1))]
+
+    scaler = StandardScaler().fit(main.iloc[:, 2:]) # standardize based on entire table
+    main.iloc[:, 2:] = scaler.transform(main.iloc[:, 2:])
 
     arr=[]
     for name, g in main.groupby('datacqtr'):
@@ -29,6 +33,7 @@ def main(sql_version=False):
         arr.append(np.array(df0))
 
     arr3d = np.array(arr)
+    print(arr3d.shape)
     return arr3d
 
 if __name__ == '__main__':
