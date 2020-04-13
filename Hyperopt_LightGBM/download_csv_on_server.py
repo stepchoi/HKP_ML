80401485ea134d2ebf35aa784139fc3573946dbb
claircocoanut <58301352+claircocoanut@users.noreply.github.com>
2020-04-13 18:00:07 +0800

    Update lightgbm

---
+++ Hyperopt_LightGBM/download_csv_on_server.py
@@ -0,0 +1,10 @@
+db_string = 'postgres://postgres:DLvalue123@hkpolyu.cgqhw7rofrpo.ap-northeast-2.rds.amazonaws.com:5432/postgres'
+engine = create_engine(db_string)
+main = pd.read_sql('SELECT * FROM main', engine)
+dep = pd.read_sql('SELECT * FROM niq_main', engine)
+macro = pd.read_sql("SELECT * FROM macro_main", engine)
+stock  = pd.read_sql("SELECT * FROM stock_main", engine)
+main.to_csv('main.csv',index=False)
+dep.to_csv('niq_main.csv',index=False)
+macro.to_csv('macro_main.csv',index=False)
+stock.to_csv('stock_main.csv',index=False)
